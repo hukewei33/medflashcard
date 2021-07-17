1ed6116d0fe007c1f57ae0ad26839721a446f278
Kenny Hu <hukewei33@gmail.com>
2021-07-17 21:09:21 +0800

    make loc, medtest and testcat name unique for csv import

--- flashCardApp/models.py
+++ flashCardApp/models.py
@@ -3,21 +3,19 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Loc(models.Model):
-    name = models.CharField(max_length=200)
-    # top = models.PositiveIntegerField(default=0)
-    # left = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=200, unique = True)
     REGION_CHOICES = [("head","head"),("forearm","forearm"),("palm","palm"),("chest","chest"),("pelvis","pelvis"),("legs","legs")]
     region = models.CharField(max_length=200,choices=REGION_CHOICES,blank=True, null=True)
     def __str__(self):
         return self.name
 
 class TestCat(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique = True)
     def __str__(self):
         return self.name
 
 class MedTest(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique = True)
     testcat = models.ForeignKey(TestCat, on_delete=models.CASCADE, blank=True, null=True)
     loc = models.ForeignKey(Loc, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
